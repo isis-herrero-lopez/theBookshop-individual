4ae57dad37e167b9db2522957a704f5d86e23faf
Your Name <you@example.com>
2020-12-03 08:52:25 +0200

    dynamically filling of productDetails info section

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 const productsList = document.getElementsByClassName("products_list")[0];
 fetch("./json/books.json")
   .then(res => {
@@ -6,28 +5,24 @@ fetch("./json/books.json")
   })
   .then(data => {
     data.map(item => {
-      const product = `<div class="product_item" onclick="openDetails('${item.title}')">
-      <img src="${item.image}">
-        <h3>${item.title}</h3>
-        <h4>${item.author}</h4>
-        <h5>${item.price} €</h5>
-        <p class="button_primary">Add to Basket</p>
+      const product = `<div class="product_item">
+        <div onclick="openDetails('${item.title}')">
+          <img src="${item.image}">
+          <h3>${item.title}</h3>
+          <h4>${item.author}</h4>
+          <h5>${item.price} €</h5>
+        </div>
+        <p class="button_primary" onclick="toBasket('${item.title}')">Add to Basket</p>
         <p style="display:none">${item.filters}</p>
     </div>`;
     productsList.innerHTML += product;
     });
   });
 
-  //onclick="toBasket('${event}', '${item.title}')"
-
 function openDetails(title) {
   location.href = "./productDetail.html?title=" + title;
 }
 
-/*
-function toBasket(event, title) {
-  console.log(event);
-  event.stopPropagation();
-  console.log("here", title);
+function toBasket(title) {
+  console.log(title);
 }
-*/
--- scripts/productDetail.js
+++ scripts/productDetail.js
@@ -1,3 +1,29 @@
+const imageSlot = document.getElementById("productImage");
+const titleSlot = document.getElementById("productTitle");
+const authorSlot = document.getElementById("productAuthor");
+const formatSlot = document.getElementById("productFormat");
+const pubDateSlot = document.getElementById("productPubDate");
+const languageSlot = document.getElementById("productLanguage");
+const lengthSlot = document.getElementById("productLength");
+const priceSlot = document.getElementById("productPrice");
+const summarySlot = document.getElementById("productSummary");
+
 const title = window.location.search.substring(7).split("%20").join(' ');
 console.log(title);
-//fetch;
+fetch("./json/books.json")
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    const product = data.filter(item => item.title === title)[0];
+    console.log(product);
+    imageSlot.src = product.image;
+    titleSlot.innerHTML = product.title;
+    authorSlot.innerHTML = product.author;
+    formatSlot.innerHTML = product.format;
+    pubDateSlot.innerHTML = product.pubDate;
+    languageSlot.innerHTML = product.language;
+    lengthSlot.innerHTML = product.length;
+    priceSlot.innerHTML = product.price.toFixed(2) + " €";
+    summarySlot.innerHTML = product.summary;
+  });
